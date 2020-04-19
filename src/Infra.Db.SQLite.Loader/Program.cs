b2a5a4705c3b6be51264616f24d1527ec84a517b
Csaba Salat <csalat@globalblue.com>
2020-04-19 18:03:48 +0200

    add loader

---
+++ src/Infra.Db.SQLite.Loader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Infra.Db.SQLite.BusinessIds;
+
+namespace Infra.Db.SQLite.Loader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var businessIdHandler = new BusinessIdHandler();
+            var start = DateTime.Now;
+            var items = new List<long>();
+            for (var i = 0; i < 100; i++)
+            {
+                items.Add(await businessIdHandler.GetAsync(CancellationToken.None));
+            }
+            var end = DateTime.Now - start;
+            var delimiter = ",";
+            Console.WriteLine($"time:{end} ids: {items.Aggregate("", (i, j) => i + delimiter + j)}");
+        }
+    }
+}

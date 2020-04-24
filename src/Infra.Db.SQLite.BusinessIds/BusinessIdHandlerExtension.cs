e4e3a3c8792699d311f3660434c4629011e45d65
Csaba Salat <csalat@globalblue.com>
2020-04-24 23:49:10 +0200

    add connection string

--- src/Infra.Db.SQLite.BusinessIds/BusinessIdHandlerExtension.cs
+++ src/Infra.Db.SQLite.BusinessIds/BusinessIdHandlerExtension.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace NetExtensions
 {
     public static class BusinessIdHandlerExtension
     {
-        public static IServiceCollection AddBusinessIdHandler(this IServiceCollection services, string connectionString) 
+        public static IServiceCollection AddBusinessIdHandler(this IServiceCollection services, string connectionString = null) 
         {
-            services.AddScoped<BusinessIdHandler>();
-            services.AddScoped<SqLiteDbRestore>();
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                services.AddScoped<BusinessIdHandler>();
+                services.AddScoped<SqLiteDbRestore>();
+            }
+            else
+            {
+                services.AddScoped(p=> new BusinessIdHandler(p.GetRequiredService<ILogger<BusinessIdHandler>>(),
+                    p.GetRequiredService<SqLiteDbRestore>(), connectionString));
+                services.AddScoped(p=> new SqLiteDbRestore(p.GetRequiredService<ILogger<SqLiteDbRestore>>(),
+                    connectionString));
+            }
             return services;
         }
     }
